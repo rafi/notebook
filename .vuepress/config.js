c5f1c626a4cbbc8a75e101ef7b108a420d1d4db6
Rafael Bodill <justrafi@gmail.com>
2018-12-27 01:15:30 +0200

    Add meta information

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,6 +7,19 @@ module.exports = {
     '@vuepress/medium-zoom': true,
     '@vuepress/google-analytics': { ga: 'UA-129679382-1' },
   },
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-120x120.png` }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-76x76.png` }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-60x60.png` }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   themeConfig: {
     repo: 'rafi/notebook',
     repoLabel: '',
